868281aa7519252b7ebe37110ba4c77c2cfcc2f5
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-03-25 17:44:07 +0200

    added actions log - show hovered squares

--- game-with-squares/src/views/ActionsLog.vue
+++ game-with-squares/src/views/ActionsLog.vue
@@ -1,11 +1,58 @@
 <template>
-  <div>
-    <h1>LOG</h1>
+  <div class="actions-log">
+    <h2>Hovered Squares:</h2>
+    <template v-if="hoveredSquares.length === 0">
+      <h3>There are no actions yet</h3>
+    </template>
+    <template v-else>
+      <div class="container">
+        <div
+          class="card"
+          :class="{
+            blue: square.hovered,
+            white: !square.hovered,
+          }"
+          v-for="(square, index) in hoveredSquares"
+          :key="index"
+        >
+          column: {{ square.x + 1 }}, row: {{ square.y + 1 }}
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    hoveredSquares: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.actions-log {
+  padding-right: 2.5rem;
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  h3 {
+    color: rgb(144, 144, 144);
+  }
+  .container {
+    max-height: 450px;
+    overflow: auto;
+  }
+  .card {
+    margin-bottom: 1.5rem;
+    padding: 1rem;
+    background-color: rgba(255, 201, 134, 0.732);
+    color: rgb(101, 47, 22);
+  }
+}
+</style>
--- game-with-squares/src/views/GameField.vue
+++ game-with-squares/src/views/GameField.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1>FIELD</h1>
-    <actions-log></actions-log>
     <template v-if="areGameModsLoading">
       <loading-spinner></loading-spinner>
     </template>
@@ -9,37 +7,54 @@
       <error-message :errorMessage="errorMessage"></error-message>
     </template>
     <template v-else>
-      <div class="field-container">
-        <div class="d-flex choose-mode-container">
-          <select
-            class="form-select"
-            id="floatingSelect"
-            aria-label="Selcet game mode"
-            v-model="selectedMode"
-          >
-            <option disablesd value="">Pick mode...</option>
-            <option v-for="mode in gameMods" :key="mode.id" :value="mode">
-              {{ mode.name }}
-            </option>
-          </select>
-          <button class="btn btn-primary" @click="generateGrid">Start</button>
-        </div>
-        <div class="grid-container">
-          <template v-if="!selectedMode || !grid">
-            <div class="empty-container">
-              <h3>Empty Field</h3>
-              <h4>Please choose the game mode</h4>
-            </div>
-          </template>
-          <template v-else>
-            <div v-for="row in grid" :key="row[0].y" class="grid-row">
-              <div
-                v-for="square in row"
-                :key="square.x"
-                class="grid-square"
-              ></div>
-            </div>
-          </template>
+      <div class="d-flex flex-row-reverse justify-content-center">
+        <actions-log :hoveredSquares="hoveredSquares"></actions-log>
+        <div class="field-container">
+          <div class="d-flex choose-mode-container">
+            <select
+              class="form-select"
+              id="floatingSelect"
+              aria-label="Selcet game mode"
+              v-model="selectedMode"
+            >
+              <option disablesd value="">Pick mode...</option>
+              <option v-for="mode in gameMods" :key="mode.id" :value="mode">
+                {{ mode.name }}
+              </option>
+            </select>
+            <template v-if="!selectedMode || grid.length === 0">
+              <button class="btn btn-primary" @click="generateGrid">
+                Start
+              </button>
+            </template>
+            <template v-else>
+              <button class="btn btn-primary" @click="generateGrid">
+                Restart
+              </button>
+            </template>
+          </div>
+          <div class="grid-container">
+            <template v-if="!selectedMode || grid.length === 0">
+              <div class="empty-container">
+                <h3>Empty Field</h3>
+                <h4>Please choose the game mode</h4>
+              </div>
+            </template>
+            <template v-else>
+              <div v-for="row in grid" :key="row[0].y" class="grid-row">
+                <div
+                  v-for="square in row"
+                  :key="square.x"
+                  class="grid-square"
+                  :class="{
+                    blue: square.hovered,
+                    white: !square.hovered,
+                  }"
+                  @mouseover="onSquareHover(square)"
+                ></div>
+              </div>
+            </template>
+          </div>
         </div>
       </div>
     </template>
@@ -60,7 +75,8 @@ export default {
   data() {
     return {
       selectedMode: "",
-      grid: null,
+      grid: [],
+      hoveredSquares: [],
     };
   },
   computed: {
@@ -73,22 +89,31 @@ export default {
   methods: {
     ...mapActions(["fetchModes"]),
     generateGrid() {
-      this.grid = this.calculateGrid();
-    },
-    calculateGrid() {
-      if (!this.selectedMode) {
-        return [];
-      }
-      const size = this.selectedMode.field;
-      const grid = [];
-      for (let i = 0; i < size; i++) {
+      this.grid = [];
+      this.hoveredSquares = [];
+
+      const fieldSize = this.selectedMode.field;
+
+      for (let i = 0; i < fieldSize; i++) {
         const row = [];
-        for (let j = 0; j < size; j++) {
-          row.push({ x: j, y: i });
+        for (let j = 0; j < fieldSize; j++) {
+          row.push({ x: j, y: i, hovered: false });
         }
-        grid.push(row);
+        this.grid.push(row);
       }
-      return grid;
+    },
+    onSquareHover(square) {
+      const squareIndexToDelete = this.hoveredSquares.findIndex(
+        (item) => item.x === square.x && item.y === square.y
+      );
+
+      if (square.hovered && squareIndexToDelete !== -1) {
+        square.hovered = false;
+        this.hoveredSquares.splice(squareIndexToDelete, 1);
+      } else {
+        square.hovered = true;
+        this.hoveredSquares.push(square);
+      }
     },
   },
   mounted() {
@@ -132,8 +157,14 @@ export default {
   .grid-square {
     width: 30px;
     height: 30px;
-    background-color: white;
     border: 1px solid gray;
+    &.blue {
+      background-color: rgb(30, 166, 250);
+    }
+
+    &.white {
+      background-color: white;
+    }
   }
   }
 }
